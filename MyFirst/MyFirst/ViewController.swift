1d962b825d083bc7a89a49bcc736271bd8fdd53a
gojiteji <h27i18@ed.cc.suzuka-ct.ac.jp>
2020-01-28 12:43:33 +0900

    upload

---
+++ MyFirst/MyFirst/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyFirst
+//
+//  Created by Koki Tanaka on 2019/11/30.
+//  Copyright © 2019 gojiteji.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        outputLabel.text="Hello Swift"
+    }
+
+    @IBOutlet weak var outputLabel: UILabel!
+    
+}
+
---
+++ annonycast/annonycast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  annonycast
+//
+//  Created by Koki Tanaka on 2019/11/17.
+//  Copyright © 2019 gojiteji.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
